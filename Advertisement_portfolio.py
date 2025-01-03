c0e1b0b808eba438972c8a4f893f4ac4f429661c
Nathaniel Sayson <nathsay23@gmail.com>
2025-01-04 01:40:46 +0800

    Update Advertisement_portfolio.py

--- Advertisement_portfolio.py
+++ Advertisement_portfolio.py
@@ -18,6 +18,8 @@ import warnings
 from streamlit_option_menu import option_menu
 from streamlit_extras.mention import mention
 from strings import home_string, System_Prompt
+from swarm import Swarm, Agent
+from openai import OpenAI
 
 warnings.filterwarnings("ignore")
 
